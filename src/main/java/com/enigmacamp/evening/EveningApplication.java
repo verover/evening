d4cbaa4776caeb544b160d69fa78ab92c6555462
Azzahra Dinda Shafira <azzahradindas@gmail.com>
2021-12-25 10:07:34 +0700

    [INITIAL COMMIT] Spring Base Project

---
+++ src/main/java/com/enigmacamp/evening/EveningApplication.java
@@ -0,0 +1,13 @@
+package com.enigmacamp.evening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigmacamp/evening/EveningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigmacamp.evening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
